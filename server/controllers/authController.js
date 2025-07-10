7ba484d679ad8a3934b189ec0bf5ea03ef8a7767
kanikapoor-dot <kanikapoor24@gmail.com>
2025-07-10 17:41:59 +0530

    Created Server with user's register and login Api's

---
+++ server/controllers/authController.js
@@ -0,0 +1,50 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const generateJWTToken = (userId) => {
+  return jwt.sign(
+    {
+      id: userId,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
+
+exports.registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const userExist = await User.findOne({ email });
+    if (userExist)
+      return res.status(400).json({ error: "User Email already exists" });
+
+    const user = await User.create({ username, email, password });
+    const userToken = generateJWTToken(user._id);
+
+    res.status(201).json({ user: { id: user._id, username, email }, token });
+  } catch (err) {
+    res.status(500).json({ error: "Registration Failed" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: "User not found!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ error: "Invalid Credentials" });
+
+    const token = generateJWTToken(user._id);
+
+    res
+      .status(200)
+      .json({ user: { id: user._id, username: user.username, email }, token });
+  } catch (err) {
+    res.status(500).json({ error: "Login Failed" });
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on mongo Atlas ${PORT}`);
+    });
+  })
+  .catch((err) => console.error("Mongo DB connection error : ", err));
