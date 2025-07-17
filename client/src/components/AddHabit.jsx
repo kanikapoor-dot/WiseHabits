92d2e4182a26997c457b2008da12772e99adf57c
kanikapoor-dot <kanikapoor24@gmail.com>
2025-07-17 16:33:50 +0530

    added outside click should close the Add habit model

--- client/src/components/AddHabit.jsx
+++ client/src/components/AddHabit.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaTimes } from "react-icons/fa";
 import api from "../api/axios";
 
@@ -7,6 +7,7 @@ const AddHabit = ({ setHabits, setShowAddHabitForm, habits }) => {
   const [description, setDescription] = useState("");
   const [frequency, setFrequency] = useState("daily");
   const [creating, setCreating] = useState(false);
+  const modelRef = useRef(null);
 
   const AddNewHabit = async (e) => {
     e.preventDefault();
@@ -33,9 +34,21 @@ const AddHabit = ({ setHabits, setShowAddHabitForm, habits }) => {
     }
   };
 
+  const handleClickOutside = (e) => {
+    if (modelRef.current && !modelRef.current.contains(e.target)) {
+      setShowAddHabitForm(false);
+    }
+  };
+
   return (
-    <div className="fixed inset-0 bg-black/70 flex justify-center items-center p-2 z-50 ">
-      <div className="bg-white p-6 rounded-xl shadow-xl w-[50%] ">
+    <div
+      className="fixed inset-0 bg-black/70 flex justify-center items-center p-2 z-50 "
+      onClick={handleClickOutside}
+    >
+      <div
+        className="bg-white p-6 rounded-xl shadow-xl w-[50%] "
+        ref={modelRef}
+      >
         <div className="flex justify-between items-center mb-3">
           <p className="text-black font-bold text-2xl">Add New Habit</p>
           <button onClick={() => setShowAddHabitForm(false)}>
@@ -43,31 +56,52 @@ const AddHabit = ({ setHabits, setShowAddHabitForm, habits }) => {
           </button>
         </div>
         <form onSubmit={AddNewHabit} className="flex flex-col gap-6">
-          <input
-            type="text"
-            placeholder="Habit Name"
-            value={name}
-            required
-            className="border border-sm rounded-sm p-2"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="description"
-            value={description}
-            required
-            className="border border-sm rounded-sm p-2"
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <select
-            value={frequency}
-            className="border border-sm rounded-sm p-2"
-            onChange={(e) => setFrequency(e.target.value)}
-          >
-            <option value="daily">Daily</option>
-            <option value="weekly">Weekly</option>
-            <option value="monthly">Monthly</option>
-          </select>
+          <div className="flex flex-col gap-2">
+            <label>Habit Name</label>
+            <input
+              type="text"
+              placeholder="Habit Name"
+              value={name}
+              required
+              className="border border-sm rounded-sm p-2"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label>Habit Description</label>{" "}
+            <input
+              type="text"
+              placeholder="description"
+              value={description}
+              required
+              className="border border-sm rounded-sm p-2"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label>Repeat</label>
+            <div className="flex gap-2">
+              <button
+                type="button"
+                className={`border px-2.5 py-2 rounded text-white ${
+                  frequency === "daily" ? "bg-orange-500" : "bg-gray-400"
+                }`}
+                onClick={(e) => setFrequency("daily")}
+              >
+                Daily
+              </button>
+              <button
+                type="button"
+                className={`border px-2.5 py-2 rounded text-white ${
+                  frequency === "weekly" ? "bg-orange-500 " : "bg-gray-400"
+                }`}
+                onClick={(e) => setFrequency("weekly")}
+              >
+                Weekly
+              </button>
+            </div>
+          </div>
+
           <button
             type="submit"
             disabled={creating}
